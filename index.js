f02cdd166962eab607b9ed24a1297cde06ddfbbb
codertinie <martin.munyao@moringaschool.com>
2022-09-15 17:35:10 +0300

    Added a fetch function to fetch data from the API

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('linked')
+  const BMSAPI = 'http://localhost:3000/Books'
+
+  fetch(BMSAPI)
+  .then((res) => res.json())
+  .then (renderGram);
+
+  function renderGram(data){
+    console.log(data);
+  }
